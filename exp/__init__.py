60de500a3d9ebf4e7195cb52b887aed5e675faf9
Master-PLC <18358716036@163.com>
2025-09-16 16:51:32 +0800

    update meta

--- exp/__init__.py
+++ exp/__init__.py
@@ -2,6 +2,7 @@ from .exp_long_term_forecasting import Exp_Long_Term_Forecast
 from .exp_long_term_forecasting_cca_loss import Exp_Long_Term_Forecast_CCA_Loss
 from .exp_long_term_forecasting_meta_reptile import Exp_Long_Term_Forecast_META_Reptile
 from .exp_long_term_forecasting_meta import Exp_Long_Term_Forecast_META
+from .exp_long_term_forecasting_meta_ml3 import Exp_Long_Term_Forecast_META_ML3
 from .exp_long_term_forecasting_ot import Exp_Long_Term_Forecast_OT
 from .exp_short_term_forecasting import Exp_Short_Term_Forecast
 
@@ -10,6 +11,7 @@ EXP_DICT = {
     'short_term_forecast': Exp_Short_Term_Forecast,
     'long_term_forecast_meta_reptile': Exp_Long_Term_Forecast_META_Reptile,
     'long_term_forecast_meta': Exp_Long_Term_Forecast_META,
+    'long_term_forecast_meta_ml3': Exp_Long_Term_Forecast_META_ML3,
     'long_term_forecast_ot': Exp_Long_Term_Forecast_OT,
     'long_term_forecast_cca_loss': Exp_Long_Term_Forecast_CCA_Loss,
 }
